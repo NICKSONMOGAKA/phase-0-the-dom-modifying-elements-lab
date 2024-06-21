8b9ecc8852d05a8b5461ae963531071225eadde4
+ <nickson.mogaka@student.moringaschool.com>
2024-06-21 14:03:27 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your code heredocument.addEventListener('DOMContentLoaded', function() {
+  const newHeader = document.createElement('h1');
+  newHeader.id = 'victory';
+  newHeader.textContent = 'John Doe is the champion';
+  document.body.appendChild(newHeader);
